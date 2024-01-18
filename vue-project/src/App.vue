38c51109d897557d8c98cdb7e403742a35482f82
Jonathan Amorim <94270784+0Neocortexx@users.noreply.github.com>
2024-01-18 14:40:48 -0300

    create first project with vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,13 @@
+<!-- Script-->
+
 <script setup>
-import hello from './components/hello.vue';
+    // Importação dos componentes
+    import Exemplo01 from './components/exemplo01Interpolacao.vue' // A palavra após o import sempre maiuscula (Boas praticas)
+    // o valor após o import é o que será usado como tag
+
 </script>
 
-
+<!-- HTML -->
 <template>
-  <hello />
+    <Exemplo01></Exemplo01>
 </template>
-
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
+// Importa um módulo chamado createApp
 import { createApp } from 'vue'
+
+// Importa um arquivo chamado App.vue
 import App from './App.vue'
 
+// Renderizar
 createApp(App).mount('#app')
